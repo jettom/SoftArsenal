f8b595c710463a4f0804ab0b0c937d7936b0c822
π <jettom@hotmail.com>
2020-07-19 14:48:30 +0900

    Create 1.两数之和.java

---
+++ leetcode/1.两数之和.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+// @lc code=end
+
